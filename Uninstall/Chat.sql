8eda15b74c5ef0e7b3e09d65e9223b3460db005e
yezhilie <1044632313@qq.com>
2018-06-27 16:47:06 +0800

    update

--- Uninstall/Chat.sql
+++ Uninstall/Chat.sql
@@ -1,3 +1,5 @@
 DROP TABLE IF EXISTS `cms_chat_room`;
 DROP TABLE IF EXISTS `cms_chat_room_person`;
 DROP TABLE IF EXISTS `cms_chat_msg`;
+DROP TABLE IF EXISTS `cms_chat_group`;
+DROP TABLE IF EXISTS `cms_chat_fans`;
